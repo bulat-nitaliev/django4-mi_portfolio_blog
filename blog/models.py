ebc8772a3efb44d88e732eaa9796b3d82d41d01f
Bulat/Nitaliev <nibulat924@gmail.com>
2023-04-25 18:17:29 +0300

    model Blog,detail

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	date = models.DateField()
+	image = models.ImageField(upload_to='blog/images')
+
+	def __str__(self):
+		return self.title
