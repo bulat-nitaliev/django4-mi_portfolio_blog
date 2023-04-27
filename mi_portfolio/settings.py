510f62ca0555f8ca41873137b129b2426458749e
bulat <nibulat@924gmail.com>
2023-04-27 14:49:43 +0000

    Remove old stuff

--- mi_portfolio/settings.py
+++ mi_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%(eb7b+ufg#-!#yf^4l#q&6&(pu3(0d)w&2c%0-qpqir=lxqd!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['abuahmad.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
